d1d399cf136a665db8b4257b70e5aff88c4ac58b
vansh1504.be21@chitkara.edu.in <vansh1504.be21@chitkara.edu.in>
2022-09-13 15:56:10 +0530

    Final website in the session

---
+++ pages/vansh.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products(props) {
+    return (
+        <div className='text-center container mx-auto px-4'>
+            <h1>Your name</h1>
+        </div>
+    )
+}
+
+export default Products
